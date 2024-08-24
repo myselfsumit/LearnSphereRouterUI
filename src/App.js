00e4f284863f55d4a24c46a42cb7a592634f2e5d
Sumit Kaushal <sumitkaushal039@gmail.com>
2024-08-24 11:50:29 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
+    <div>
+      <Navbar/>
 
+      <Routes>
+        <Route path="/" elements={<Home/>} />
+        <Route path="/login" elements={<Login/>} />
+        <Route path="/signup" elements={<Signup/>} />
+        <Route path="/dashboard" elements={<Dashboard/>} />
+      </Routes>
+
+    </div>
+            
   )
 }
 
